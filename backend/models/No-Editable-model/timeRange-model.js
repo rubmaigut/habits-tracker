0b3a79b84a7b4c831b66ff3ddb22a5b0c9a3f47b
rubmaigut <rubmaigut.7@gmail.com>
2021-06-16 09:57:42 +0200

    set data Time range

--- backend/models/No-Editable-model/timeRange-model.js
+++ backend/models/No-Editable-model/timeRange-model.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose")
+const dataTimeRange = require("../../frequencies.json")
 
 const TimeRange = mongoose.model('TimeRange',{
     timeRangeName: String,
 })
+
+if (process.env.RESET_DB) {
+	const seedTimeDatabase = async () => {
+    await TimeRange.deleteMany({})
+
+    dataTimeRange.forEach((timeData) => {
+			new TimeRange(timeData).save()
+		})
+  }
+
+  seedTimeDatabase()
+}
 module.exports = TimeRange;
   
