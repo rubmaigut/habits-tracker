2d13761e4527f5faa7afe22c40c28b22504fc53f
rubmaigut <rubmaigut.7@gmail.com>
2021-06-17 23:25:23 +0200

    cors test

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(function(req, res, next) {
 
 app.use(cors());
 // Add middlewares to enable cors and json body parsing
-app.use(cors({origin:"http://localhost:3000/", credentials: true, crossdomain: true}));
+//app.use(cors({origin:"http://localhost:3000/", credentials: true, crossdomain: true}));
 app.use(express.json());
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
